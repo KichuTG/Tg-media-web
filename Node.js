261e6536213bdea38b3164f719ca8fa59def0ca3
Kichu <kichumon333@outlook.com>
2025-05-03 14:27:00 +0530

    Update Node.js

--- Node.js
+++ Node.js
@@ -58,7 +58,7 @@ app.get('/api/search', async (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 connectToDatabase().then(() => {
     app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
