827078a912a9856f2eb5da8f05b986185888a5ec
Kichu <kichumon333@outlook.com>
2025-05-03 14:26:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -5,13 +5,13 @@ load_dotenv()
 
 class Config:
     # Telegram Bot Configuration
-    BOT_USERNAME = os.getenv("BOT_USERNAME", "DramaShip1bot")
+    BOT_USERNAME = os.getenv("BOT_USERNAME", "@TGNETFLIX1BOT")
     
     # Database Configuration
-    DATABASE_URI = os.getenv("DATABASE_URI", "mongodb://localhost:27017")
-    DATABASE_NAME = os.getenv("DATABASE_NAME", "telegram_files")
-    COLLECTION_NAME = os.getenv("COLLECTION_NAME", "media_files")
+    DATABASE_URI = os.getenv("DATABASE_URI", "mongodb+srv://jiosaavn:jiosaavn@cluster0.ouhhe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_NAME = os.getenv("DATABASE_NAME", "PIRO")
+    COLLECTION_NAME = os.getenv("COLLECTION_NAME", "FILES")
     
     # Search Configuration
     USE_CAPTION_FILTER = bool(os.getenv("USE_CAPTION_FILTER", True))
-    MAX_B_TN = os.getenv("MAX_B_TN", 10)  # Maximum buttons per page
+    MAX_B_TN = os.getenv("MAX_B_TN", 50)  # Maximum buttons per page
